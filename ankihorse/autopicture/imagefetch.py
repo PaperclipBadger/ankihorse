1505744b3b0725f6386d96527891809e4942f6dc
Blaine Rogers <blaine.w.rogers@gmail.com>
2016-05-10 01:09:25 +0100

    Moved unit tests out from __name__ == '__main__' and updated mock patching.

--- ankihorse/autopicture/imagefetch.py
+++ ankihorse/autopicture/imagefetch.py
@@ -5,7 +5,7 @@ import os
 
 from aqt import mw
 
-from updateraddon import FieldUpdater
+from ..updateraddon import FieldUpdater
 
 
 class GoogleImageFieldUpdater(FieldUpdater):
@@ -136,111 +136,108 @@ class GoogleImageFieldUpdater(FieldUpdater):
         return result['items'][0]['link']
 
 
-if __name__ == '__main__':
-    import unittest
-    import mock
+# unit tests
+import unittest
+import mock
 
-    import random
-    import string
-
-    
-    def randomstring(n):
-        """Generates a string of n random lower case letters."""
-        rand_chr = lambda: random.SystemRandom().choice(string.ascii_lowercase)
-        return ''.join(rand_chr() for _ in range(30))
+import random
+import string
 
 
-    class InitialiseTestCase(unittest.TestCase):
-
-        def setUp(self):
-            self.query_field = randomstring(30)
-            self.target_field = randomstring(30)
-
-        def testQueryField(self):
-            g = GoogleImageFieldUpdater(self.query_field, '')
-            self.assertEqual(g.sourceFields(), [self.query_field])
-
-        def testTargetField(self):
-            g = GoogleImageFieldUpdater('', self.target_field)
-            self.assertEqual(g.targetFields(), [self.target_field])
-
-        def testBoth(self):
-            g = GoogleImageFieldUpdater(self.query_field, self.target_field)
-            self.assertEqual(g.sourceFields(), [self.query_field])
-            self.assertEqual(g.targetFields(), [self.target_field])
+def randomstring(n):
+    """Generates a string of n random lower case letters."""
+    rand_chr = lambda: random.SystemRandom().choice(string.ascii_lowercase)
+    return ''.join(rand_chr() for _ in range(30))
 
 
-    @mock.patch('__main__.mw')
-    class modifyFieldsTestCase(unittest.TestCase):
+class InitialiseTestCase(unittest.TestCase):
 
-        def setUp(self, *mocks):
-            ## create temp file
-            filename = randomstring(10) + '.jpg'
-            basedir = GoogleImageFieldUpdater.DIRECTORY
-            self.temp_path = os.path.join(basedir, filename)
-            with open(self.temp_path, 'a'):
-                pass
+    def setUp(self):
+        self.query_field = randomstring(30)
+        self.target_field = randomstring(30)
 
-            ## create test object and patch methods
-            self.g = GoogleImageFieldUpdater(randomstring(9), randomstring(9))
-            self.g.downloadImageFromURL = mock.MagicMock()
-            self.g.downloadImageFromURL.return_value = self.temp_path
-            self.g.firstImageFromGoogle = mock.MagicMock()
-            self.note = mock.MagicMock()
+    def testQueryField(self):
+        g = GoogleImageFieldUpdater(self.query_field, '')
+        self.assertEqual(g.sourceFields(), [self.query_field])
 
-        def tearDown(self, *mocks):
-            ## delete temp file if it's still there
-            if os.path.isfile(self.temp_path):
-                os.remove(self.temp_path)
+    def testTargetField(self):
+        g = GoogleImageFieldUpdater('', self.target_field)
+        self.assertEqual(g.targetFields(), [self.target_field])
 
-        def testNotModifiedOnUrlFetchFailure(self, mock_mw):
-            self.g.firstImageFromGoogle.return_value = None
-
-            self.assertFalse(self.g.modifyFields(self.note))
-            self.assertFalse(self.note.__setitem__.called)
-
-        def testNotModifiedOnImageFetchFailure(self, mock_mw):
-            self.g.downloadImageFromURL.return_value = None
-
-            self.assertFalse(self.g.modifyFields(self.note))
-            self.assertFalse(self.note.__setitem__.called)
-
-        def testNotModifiedOnBlankQuery(self, mock_mw):
-            mock_mw.col.media.strip.return_value = ''
-
-            self.assertFalse(self.g.modifyFields(self.note))
-            self.assertFalse(self.note.__setitem__.called)
-
-        def testModifiedCorrectly(self, mock_mw):
-            self.assertTrue(self.g.modifyFields(self.note))
-
-            target = self.g.targetFields()[0]
-            filename = os.path.basename(self.temp_path)
-            image_tag = u'<img src="{}" />'.format(filename)
-            self.assertTrue(self.note.__setitem__.called_once_with \
-                            (target, image_tag))
-
-        def testAddToMediaDatabase(self, mock_mw):
-            self.assertTrue(self.g.modifyFields(self.note))
-            self.assertTrue(mw.col.media.addFile.called)
-
-        def testTempFileRemoved(self, mock_mw):
-            self.g.modifyFields(self.note)
-            self.assertFalse(os.path.isfile(self.temp_path))
+    def testBoth(self):
+        g = GoogleImageFieldUpdater(self.query_field, self.target_field)
+        self.assertEqual(g.sourceFields(), [self.query_field])
+        self.assertEqual(g.targetFields(), [self.target_field])
 
 
-    @mock.patch('urllib.urlretrieve')
-    class DownloadImageFromURLTestCase(unittest.TestCase):
+@mock.patch('{}.mw'.format(__name__))
+class modifyFieldsTestCase(unittest.TestCase):
 
-        def testFileName(self, mock_retrieve):
-            website = 'http://' + randomstring(10) + '.com/'
-            remote_image = randomstring(10) + '.jpg'
-            url = website + remote_image
+    def setUp(self, *mocks):
+        ## create temp file
+        filename = randomstring(10) + '.jpg'
+        basedir = GoogleImageFieldUpdater.DIRECTORY
+        self.temp_path = os.path.join(basedir, filename)
+        with open(self.temp_path, 'a'):
+            pass
 
-            filename = GoogleImageFieldUpdater.downloadImageFromURL(url)
+        ## create test object and patch methods
+        self.g = GoogleImageFieldUpdater(randomstring(9), randomstring(9))
+        self.g.downloadImageFromURL = mock.MagicMock()
+        self.g.downloadImageFromURL.return_value = self.temp_path
+        self.g.firstImageFromGoogle = mock.MagicMock()
+        self.note = mock.MagicMock()
 
-            basedir = GoogleImageFieldUpdater.DIRECTORY
-            self.assertEqual(filename, os.path.join(basedir, remote_image))
-            
+    def tearDown(self, *mocks):
+        ## delete temp file if it's still there
+        if os.path.isfile(self.temp_path):
+            os.remove(self.temp_path)
 
-    unittest.main()
+    def testNotModifiedOnUrlFetchFailure(self, mock_mw):
+        self.g.firstImageFromGoogle.return_value = None
+
+        self.assertFalse(self.g.modifyFields(self.note))
+        self.assertFalse(self.note.__setitem__.called)
+
+    def testNotModifiedOnImageFetchFailure(self, mock_mw):
+        self.g.downloadImageFromURL.return_value = None
+
+        self.assertFalse(self.g.modifyFields(self.note))
+        self.assertFalse(self.note.__setitem__.called)
+
+    def testNotModifiedOnBlankQuery(self, mock_mw):
+        mock_mw.col.media.strip.return_value = ''
+
+        self.assertFalse(self.g.modifyFields(self.note))
+        self.assertFalse(self.note.__setitem__.called)
+
+    def testModifiedCorrectly(self, mock_mw):
+        self.assertTrue(self.g.modifyFields(self.note))
+
+        target = self.g.targetFields()[0]
+        filename = os.path.basename(self.temp_path)
+        image_tag = u'<img src="{}" />'.format(filename)
+        self.assertTrue(self.note.__setitem__.called_once_with \
+                        (target, image_tag))
+
+    def testAddToMediaDatabase(self, mock_mw):
+        self.assertTrue(self.g.modifyFields(self.note))
+        self.assertTrue(mw.col.media.addFile.called)
+
+    def testTempFileRemoved(self, mock_mw):
+        self.g.modifyFields(self.note)
+        self.assertFalse(os.path.isfile(self.temp_path))
+
+
+@mock.patch('urllib.urlretrieve')
+class DownloadImageFromURLTestCase(unittest.TestCase):
+
+    def testFileName(self, mock_retrieve):
+        website = 'http://' + randomstring(10) + '.com/'
+        remote_image = randomstring(10) + '.jpg'
+        url = website + remote_image
+
+        filename = GoogleImageFieldUpdater.downloadImageFromURL(url)
+
+        basedir = GoogleImageFieldUpdater.DIRECTORY
+        self.assertEqual(filename, os.path.join(basedir, remote_image))
--- ankihorse/updateraddon.py
+++ ankihorse/updateraddon.py
@@ -166,171 +166,168 @@ class Addon():
                 self._field_updater.modifyFields(mw.col.getNote(nid))
         showInfo("Done regenerating images!")
 
+
 # unit tests
-if __name__ == '__main__':
-
-    import unittest
-    import mock
+import unittest
+import mock
 
 
-    class TestFieldUpdater(FieldUpdater):
-        """Field updater for testing purposes."""
+class TestFieldUpdater(FieldUpdater):
+    """Field updater for testing purposes."""
 
-        def __init__(self):
-            self.modify_return_value = False
+    def __init__(self):
+        self.modify_return_value = False
 
-        @staticmethod
-        def validModel():
-            return { 'name': 'testName' 
-                   , 'flds': [ { 'name': 'src1' }
-                             , { 'name': 'src2' }
-                             , { 'name': 'tgt1' } 
-                             , { 'name': 'tgt2' }
-                             ]
-                   }
-        
-        def sourceFields(self):
-            return ['src1', 'src2']
-
-        def targetFields(self):
-            return ['tgt1', 'tgt2']
-
-        def modifyFields(self, note):
-            return self.modify_return_value
-
-
-    def fieldNames(model):
-        """To be substituted for mw.col.models.fieldNames"""
-        return [ f['name'] for f in model['flds'] ]
-
-
-    @mock.patch('__main__.SIGNAL')
-    @mock.patch('__main__.QAction')
-    @mock.patch('__main__.addHook')
-    @mock.patch('__main__.mw')
-    class InitialiseTestCase(unittest.TestCase):
-        
-        def testAddHook(self, mock_mw, mock_addHook, *unused):
-            a = Addon(TestFieldUpdater(), 'test')
-            mock_addHook.assert_called_once_with\
-                    ('editFocusLost', a.onFocusLost)
-
-        def testAddMenuItem(self, mock_mw, *args):
-            a = Addon(TestFieldUpdater(), 'test')
-            self.assertTrue(mock_mw.connect.called)
-            self.assertTrue(mock_mw.form.menuTools.addAction.called)
+    @staticmethod
+    def validModel():
+        return { 'name': 'testName' 
+               , 'flds': [ { 'name': 'src1' }
+                         , { 'name': 'src2' }
+                         , { 'name': 'tgt1' } 
+                         , { 'name': 'tgt2' }
+                         ]
+               }
     
+    def sourceFields(self):
+        return ['src1', 'src2']
 
-    @mock.patch('__main__.SIGNAL')
-    @mock.patch('__main__.QAction')
-    @mock.patch('__main__.addHook')
-    @mock.patch('__main__.mw')
-    class ShouldModifyTestCase(unittest.TestCase):
+    def targetFields(self):
+        return ['tgt1', 'tgt2']
 
-
-        def testHasJustSourceFields(self, mock_mw, *unused):
-            mock_mw.col.models.fieldNames = fieldNames
-            model = { 'name': 'testName'
-                    , 'flds': [ { 'name': 'src1' }
-                              , { 'name': 'src2' }
-                              ]
-                    }
-            
-            a = Addon(TestFieldUpdater(), 'test')
-            self.assertFalse(a.shouldModify(model))
-            
-        def testHasJustTargetFields(self, mock_mw, *unused):
-            mock_mw.col.models.fieldNames = fieldNames
-            model = { 'name': 'testName'
-                    , 'flds': [ { 'name': 'tgt1' }
-                              , { 'name': 'tgt2' }
-                              ]
-                    }
-
-            a = Addon(TestFieldUpdater(), 'test')
-            self.assertFalse(a.shouldModify(model))
-
-        def testHasRequiredFields(self, mock_mw, *unused):
-            mock_mw.col.models.fieldNames = fieldNames
-            model = TestFieldUpdater.validModel()
-
-            a = Addon(TestFieldUpdater(), 'test')
-            self.assertTrue(a.shouldModify(model))
-
-        def testNameContainsSubstring(self, mock_mw, *unused):
-            mock_mw.col.models.fieldNames = fieldNames
-            model = TestFieldUpdater.validModel()
-
-            a = Addon(TestFieldUpdater(), 'test', 'test')
-            self.assertTrue(a.shouldModify(model))
-
-        def testDoesNotHaveSourceFields(self, mock_mw, *unused):
-            mock_mw.col.models.fieldNames = fieldNames
-            model = TestFieldUpdater.validModel()
-
-            a = Addon(TestFieldUpdater(), 'test', 'horse')
-            self.assertFalse(a.shouldModify(model))
+    def modifyFields(self, note):
+        return self.modify_return_value
 
 
-    @mock.patch('__main__.mw')
-    class ModifyFieldsTestCase(unittest.TestCase):
+def fieldNames(model):
+    """To be substituted for mw.col.models.fieldNames"""
+    return [ f['name'] for f in model['flds'] ]
 
-        @mock.patch('__main__.SIGNAL')
-        @mock.patch('__main__.QAction')
-        @mock.patch('__main__.addHook')
-        @mock.patch('__main__.mw')
-        def setUp(self, *mocks):
-            self.updater = TestFieldUpdater()
-            self.addon = Addon(self.updater, 'test')
+
+@mock.patch('{}.SIGNAL'.format(__name__))
+@mock.patch('{}.QAction'.format(__name__))
+@mock.patch('{}.addHook'.format(__name__))
+@mock.patch('{}.mw'.format(__name__))
+class InitialiseTestCase(unittest.TestCase):
+    
+    def testAddHook(self, mock_mw, mock_addHook, *unused):
+        a = Addon(TestFieldUpdater(), 'test')
+        mock_addHook.assert_called_once_with\
+                ('editFocusLost', a.onFocusLost)
+
+    def testAddMenuItem(self, mock_mw, *args):
+        a = Addon(TestFieldUpdater(), 'test')
+        self.assertTrue(mock_mw.connect.called)
+        self.assertTrue(mock_mw.form.menuTools.addAction.called)
+
+
+@mock.patch('{}.SIGNAL'.format(__name__))
+@mock.patch('{}.QAction'.format(__name__))
+@mock.patch('{}.addHook'.format(__name__))
+@mock.patch('{}.mw'.format(__name__))
+class ShouldModifyTestCase(unittest.TestCase):
+
+
+    def testHasJustSourceFields(self, mock_mw, *unused):
+        mock_mw.col.models.fieldNames = fieldNames
+        model = { 'name': 'testName'
+                , 'flds': [ { 'name': 'src1' }
+                          , { 'name': 'src2' }
+                          ]
+                }
         
-        def testInvalidModel(self, mock_mw):
-            mock_mw.col.models.fieldNames = fieldNames
-            note = mock.Mock()
-            note.model.return_value = { 'name': 'testName' , 'flds': [ ] }
-
-            self.updater.modify_return_value = True
-            self.assertFalse(self.addon.modifyFields(note))
-            self.assertEqual(note.mock_calls, [mock.call.model()])
-            
-        def testValidModel(self, mock_mw):
-            mock_mw.col.models.fieldNames = fieldNames
-            note = mock.Mock()
-            note.model.return_value = TestFieldUpdater.validModel()
-
-            self.updater.modify_return_value = True
-            self.assertTrue(self.addon.modifyFields(note))
-            self.updater.modify_return_value = False
-            self.assertFalse(self.addon.modifyFields(note))
-
-
-    class OnFocusLostTestCase(unittest.TestCase):
-
-        @mock.patch('__main__.SIGNAL')
-        @mock.patch('__main__.QAction')
-        @mock.patch('__main__.addHook')
-        @mock.patch('__main__.mw')
-        def setUp(self, *mocks):
-            self.addon = Addon(TestFieldUpdater(), 'test')
-            self.addon.modifyFields = mock.Mock()
+        a = Addon(TestFieldUpdater(), 'test')
+        self.assertFalse(a.shouldModify(model))
         
-        def testNonSourceField(self):
-            note = mock.Mock()
-            note.model.return_value = TestFieldUpdater.validModel()
-            self.addon.modifyFields.return_value = False
+    def testHasJustTargetFields(self, mock_mw, *unused):
+        mock_mw.col.models.fieldNames = fieldNames
+        model = { 'name': 'testName'
+                , 'flds': [ { 'name': 'tgt1' }
+                          , { 'name': 'tgt2' }
+                          ]
+                }
 
-            self.assertTrue(self.addon.onFocusLost(True, note, 2))
-            self.assertFalse(self.addon.onFocusLost(False, note, 2))
-            
-        def testSourceField(self):
-            note = mock.Mock()
-            note.model.return_value = TestFieldUpdater.validModel()
+        a = Addon(TestFieldUpdater(), 'test')
+        self.assertFalse(a.shouldModify(model))
 
-            self.addon.modifyFields.return_value = True
-            self.assertTrue(self.addon.onFocusLost(False, note, 0))
-            self.addon.modifyFields.return_value = False
-            self.assertFalse(self.addon.onFocusLost(False, note, 0))
-            
+    def testHasRequiredFields(self, mock_mw, *unused):
+        mock_mw.col.models.fieldNames = fieldNames
+        model = TestFieldUpdater.validModel()
 
-    #TODO: tests for regenerateAll
+        a = Addon(TestFieldUpdater(), 'test')
+        self.assertTrue(a.shouldModify(model))
 
-    unittest.main()
+    def testNameContainsSubstring(self, mock_mw, *unused):
+        mock_mw.col.models.fieldNames = fieldNames
+        model = TestFieldUpdater.validModel()
+
+        a = Addon(TestFieldUpdater(), 'test', 'test')
+        self.assertTrue(a.shouldModify(model))
+
+    def testDoesNotHaveSourceFields(self, mock_mw, *unused):
+        mock_mw.col.models.fieldNames = fieldNames
+        model = TestFieldUpdater.validModel()
+
+        a = Addon(TestFieldUpdater(), 'test', 'horse')
+        self.assertFalse(a.shouldModify(model))
+
+
+@mock.patch('{}.mw'.format(__name__))
+class ModifyFieldsTestCase(unittest.TestCase):
+
+    @mock.patch('{}.SIGNAL'.format(__name__))
+    @mock.patch('{}.QAction'.format(__name__))
+    @mock.patch('{}.addHook'.format(__name__))
+    @mock.patch('{}.mw'.format(__name__))
+    def setUp(self, *mocks):
+        self.updater = TestFieldUpdater()
+        self.addon = Addon(self.updater, 'test')
+    
+    def testInvalidModel(self, mock_mw):
+        mock_mw.col.models.fieldNames = fieldNames
+        note = mock.Mock()
+        note.model.return_value = { 'name': 'testName' , 'flds': [ ] }
+
+        self.updater.modify_return_value = True
+        self.assertFalse(self.addon.modifyFields(note))
+        self.assertEqual(note.mock_calls, [mock.call.model()])
+        
+    def testValidModel(self, mock_mw):
+        mock_mw.col.models.fieldNames = fieldNames
+        note = mock.Mock()
+        note.model.return_value = TestFieldUpdater.validModel()
+
+        self.updater.modify_return_value = True
+        self.assertTrue(self.addon.modifyFields(note))
+        self.updater.modify_return_value = False
+        self.assertFalse(self.addon.modifyFields(note))
+
+
+class OnFocusLostTestCase(unittest.TestCase):
+
+    @mock.patch('{}.SIGNAL'.format(__name__))
+    @mock.patch('{}.QAction'.format(__name__))
+    @mock.patch('{}.addHook'.format(__name__))
+    @mock.patch('{}.mw'.format(__name__))
+    def setUp(self, *mocks):
+        self.addon = Addon(TestFieldUpdater(), 'test')
+        self.addon.modifyFields = mock.Mock()
+    
+    def testNonSourceField(self):
+        note = mock.Mock()
+        note.model.return_value = TestFieldUpdater.validModel()
+        self.addon.modifyFields.return_value = False
+
+        self.assertTrue(self.addon.onFocusLost(True, note, 2))
+        self.assertFalse(self.addon.onFocusLost(False, note, 2))
+        
+    def testSourceField(self):
+        note = mock.Mock()
+        note.model.return_value = TestFieldUpdater.validModel()
+
+        self.addon.modifyFields.return_value = True
+        self.assertTrue(self.addon.onFocusLost(False, note, 0))
+        self.addon.modifyFields.return_value = False
+        self.assertFalse(self.addon.onFocusLost(False, note, 0))
+        
+
+#TODO: tests for regenerateAll
