c84f1857f139f47f55e314847d1df89ee0ee6c0a
Blaine Rogers <blaine.w.rogers@gmail.com>
2016-05-10 01:10:04 +0100

    Removed unecessary import.

--- ankihorse/autopicture/__init__.py
+++ ankihorse/autopicture/__init__.py
@@ -5,9 +5,9 @@ autopicture
 Addon that automatically adds pictures to anki cards based on a field.
 
 """
-from ..updateraddon import Addon, FieldUpdater
+from ..updateraddon import Addon
 from .imagefetch import GoogleImageFieldUpdater
 
-def initialize(name, source_field, target_field, model_name_substring=None)
+def initialize(name, source_field, target_field, model_name_substring=None):
     field_updater = GoogleImageFieldUpdater(source_field, target_field)
     Addon(field_updater, name, model_name_substring=model_name_substring)
